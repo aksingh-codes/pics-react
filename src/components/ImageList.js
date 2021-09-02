dd3bd2c3efebab3f4e5c9efcb14e0d5c0b4dc2e4
aksingh47 <56232421+aksingh47@users.noreply.github.com>
2021-09-02 11:00:47 +0530

    add grid funtionality with dynamic height cards

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,10 +1,12 @@
 import React from "react";
+import "./ImageList.css"
+import ImageCard from "./ImageCard"
 
 const ImageList = (props) => {
-  const images = props.images.map(({ description, id, urls }) => (
-    <img alt={description} key={id} src={urls.regular} />
+  const images = props.images.map((image) => (
+    <ImageCard key={image.id} image={image} />
   ));
-  return <div>{images}</div>;
+  return <div className="image-list">{images}</div>;
 };
 
 export default ImageList;
